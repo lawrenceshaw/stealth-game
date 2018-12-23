f471112f6fa5a4a23192c35a54f53474f6bd5b43
Lawrence Shaw <lawrence_shaw@hotmail.co.uk>
2018-12-23 23:42:32 +0000

    Adding script to move player using wasd or arrow keys

---
+++ stealth-game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+
+    private Rigidbody2D rb;
+    private Vector2 movementVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movementVelocity = movementDirection.normalized * Speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movementVelocity * Time.fixedDeltaTime);
+    }
+}
