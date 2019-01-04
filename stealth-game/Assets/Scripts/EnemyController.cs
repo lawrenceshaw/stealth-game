6453c0dbab2b4486f7646b9bc243a0d31b6c34e0
lawrence-shaw <lawrence_shaw@hotmail.co.uk>
2019-01-04 18:13:46 +0000

    Beginnings of an enemy script

---
+++ stealth-game/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private NavAgent navAgent;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        navAgent = GetComponent<NavAgent>();
+        navAgent.SetTarget(new Vector2(0, 5));
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
